4b5db8a8e35f1910ea2b23cd477501de4e9e7061
moeebii <ebiim621@newschool.edu>
2024-03-07 05:52:05 -0500

    f2g

--- doodle/script.js
+++ doodle/script.js
@@ -1,48 +1,56 @@
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", function() {
+  const text = "congrats! you found me! doodle here:";
+  const writeupElement = document.getElementById("typewriter");
 
-const init = () => {
-    gsap.registerPlugin(ScrollTrigger);
-    var scrollElement = document.getElementById("scrollContainer");
-
-    scrollElement.addEventListener('mouseover', function() {
-        handleMouseOver(scrollElement);
-    });
-
-    scrollElement.addEventListener('mouseout', function() {
-        handleMouseOut(scrollElement);
-    });
-
-    runLenis();
-}
-
-function handleMouseOver(element) {
-    element.classList.add("paused");
-};
-  
-function handleMouseOut(element) {
-    element.classList.remove("paused");
-};
-
-var scrollElement = document.getElementById("scrollContainer");
-
-const runLenis = () => {
-    const lenis = new Lenis({
-        infinite: true,
-    });
-    
-    function raf(time) {
-        lenis.raf(time);
-        ScrollTrigger.update();
-        requestAnimationFrame(raf);
+  function typeWriterEffect(text, element, speed) {
+    let i = 0;
+    function type() {
+      if (i < text.length) {
+        element.innerHTML += text.charAt(i);
+        i++;
+        setTimeout(type, speed);
+      }
     }
+    type();
+  }
 
-    const st = ScrollTrigger.create({
-        animation: lenis,
-        trigger: scrollElement,
-        scroller: scrollElement,
-        start: 'top top',
-        end: 'bottom top',
-    });
-    
-    requestAnimationFrame(raf);
-}
+  // Adjust the speed (second parameter) to control the typing speed
+  typeWriterEffect(text, writeupElement, 100);
+});
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  const canvas = document.getElementById("doodleCanvas");
+  const context = canvas.getContext("2d");
+  const container = document.body;
+
+  let isDoodling = false;
+  let lastX, lastY;
+
+  container.addEventListener("mousedown", startDoodle);
+  container.addEventListener("mousemove", draw);
+  container.addEventListener("mouseup", stopDoodle);
+
+  function startDoodle(event) {
+    isDoodling = true;
+    [lastX, lastY] = [event.clientX, event.clientY];
+  }
+
+  function draw(event) {
+    if (!isDoodling) return;
+
+    context.beginPath();
+    context.moveTo(lastX, lastY);
+    context.lineTo(event.clientX, event.clientY);
+    context.strokeStyle = "#b70000";
+    context.lineWidth = 5;
+    context.lineCap = "round";
+    context.stroke();
+
+    [lastX, lastY] = [event.clientX, event.clientY];
+  }
+
+  function stopDoodle() {
+    isDoodling = false;
+  }
+});
